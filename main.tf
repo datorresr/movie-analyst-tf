9ffead8e62779f4549e1dfd92ec754ceaf8af3a9
datorresr <datorresr@gmail.com>
2023-09-28 16:30:29 -0500

    Terraform main settings

--- main.tf
+++ main.tf
@@ -1,5 +1,17 @@
+terraform {
+  required_version = ">= 1.0.0, < 2.0.0"
 
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
 
+provider "aws" {
+  region = "us-east-2"
+}
 
 resource "aws_vpc" "VPCDevOpsRampUp" {
   cidr_block       = "10.1.0.0/16"
