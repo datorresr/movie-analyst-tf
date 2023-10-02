0dc0f19abbc431ea1f71b6fd11c51a214e491c47
datorresr <datorresr@gmail.com>
2023-10-02 12:12:10 -0500

    Database creation

--- main.tf
+++ main.tf
@@ -282,3 +282,29 @@ resource "aws_security_group" "SG_RDS" {
 
 }
 
+resource "aws_db_instance" "MoviesDB" {
+  engine               = "mysql"
+  identifier           = "moviesdb"
+  allocated_storage    =  20
+  engine_version       = "8.0.33"
+  instance_class       = "db.t3.micro"
+  username             = "applicationuser"
+  password             = "applicationuser"
+  parameter_group_name = "default.mysql8.0"
+  availability_zone = "us-east-1a"
+  db_name = "movie_db"
+  vpc_security_group_ids = ["${aws_security_group.SG_RDS.id}"]
+  skip_final_snapshot  = true
+  publicly_accessible =  false
+}
+
+resource "null_resource" "execute_sql" {
+  depends_on = [aws_db_instance.MoviesDB]
+
+  triggers = {
+    instance_id = aws_db_instance.MoviesDB.id
+  }
+  provisioner "local-exec" {
+    command = "mysql -h ${aws_db_instance.MoviesDB.endpoint} -P ${aws_db_instance.MoviesDB.port} -u ${aws_db_instance.MoviesDB.username} -p${aws_db_instance.MoviesDB.password} < table_creation.sql"
+  }
+}
