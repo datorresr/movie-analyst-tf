2bfbef66677b39bfc64c03501a2d2edfcd1092c8
datorresr <datorresr@gmail.com>
2023-10-04 17:08:20 -0500

    change tfstate location

--- main.tf
+++ main.tf
@@ -7,6 +7,18 @@ terraform {
       version = "~> 4.0"
     }
   }
+
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "terraform-state-movieapp"
+    key            = "global/s3/terraform.tfstate"
+    region         = "us-east-1"
+
+    # Replace this with your DynamoDB table name!
+    dynamodb_table = "terraform-movieapp"
+    encrypt        = true
+  }
+  
 }
 
 provider "aws" {
