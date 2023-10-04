075f36bfe604ca05d3732965f01f77337fb06927
datorresr <datorresr@gmail.com>
2023-10-04 13:33:31 -0500

    outputs

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "alb_dns_name" {
   value       = aws_lb.MoviesLBFrontEnd.dns_name
-  description = "The domain name of the load balancer"
+  description = "The domain name of the load balancer is"
 }
