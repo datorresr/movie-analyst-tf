d3352299b0884084e4c43bc6d833b5d274f6c9eb
datorresr <datorresr@gmail.com>
2023-10-05 17:53:59 -0500

    sensitive values

--- stage/database/outputs.tf
+++ stage/database/outputs.tf
@@ -1,9 +1,12 @@
 output "moviesDB_address" {
   value = aws_db_instance.MoviesDB.address
+  sensitive = true
 }
 output "moviesDB_username" {
   value = aws_db_instance.MoviesDB.username
+  sensitive = true
 }
 output "moviesDB_password" {
   value = aws_db_instance.MoviesDB.password
+  sensitive = true
 }
