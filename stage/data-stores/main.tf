c9439bd2df65d17bb9ac1ed83e0634125f7eb43b
datorresr <datorresr@gmail.com>
2023-10-04 20:11:26 -0500

    change folders

--- stage/data-stores/main.tf
+++ stage/data-stores/main.tf
@@ -1,3 +1,36 @@
+terraform {
+
+  required_version = ">= 1.0.0, < 2.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "terraform-state-movieapp"
+    key            = "stage/data-stores/terraform.tfstate"
+    region         = "us-east-2"
+
+    # Replace this with your DynamoDB table name!
+    dynamodb_table = "terraform-movieapp"
+    encrypt        = true
+  }
+
+
+
+
+}
+
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+
 resource "aws_db_subnet_group" "movies_RDS_SNG" {
   name       = "main"
   subnet_ids = [aws_subnet.PriBE1.id, aws_subnet.PriBE2.id]
