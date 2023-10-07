e00e26246bbeb28e60841ef39d571514f97f7d1c
datorresr <datorresr@gmail.com>
2023-10-06 19:05:04 -0500

    modules

--- stage/outputs.tf
+++ stage/outputs.tf
@@ -1,10 +1,10 @@
 output "subnet_PriBE1_id" {
-  value = module.stage_network.subnet_PriBE1_id
+  value = module.network.subnet_PriBE1_id
 }
 output "subnet_PriBE2_id" {
-  value = module.stage_network.subnet_PriBE2_id
+  value = module.network.subnet_PriBE2_id
 }
 output "SG_RDS_id" {
-  value = module.stage_network.SG_RDS_id
+  value = module.network.SG_RDS_id
 }
 
