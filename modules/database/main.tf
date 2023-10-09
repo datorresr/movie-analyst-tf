b5a6cab30860b77a11b585044401986dcff82490
datorresr <datorresr@gmail.com>
2023-10-09 15:50:46 -0500

    backend

--- modules/database/main.tf
+++ modules/database/main.tf
@@ -1,13 +1,4 @@
 terraform {
-  required_version = ">= 1.0.0, < 2.0.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-
   backend "s3" {
     # Replace this with your bucket name!
     bucket         = "terraform-state-movieapp"
--- stage/main.tf
+++ stage/main.tf
@@ -1,6 +1,31 @@
 provider "aws" {
   region = "us-east-1"
 }
+
+terraform {
+  required_version = ">= 1.0.0, < 2.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "terraform-state-movieapp"
+    key            = "stage/terraform.tfstate"
+    region         = "us-east-1"
+
+    # Replace this with your DynamoDB table name!
+    dynamodb_table = "terraform-movieapp"
+    encrypt        = true
+  }
+  
+}
+
+
 module "stage_network" {
   source = "../modules/network"
   AZ_A = "us-east-1a"
