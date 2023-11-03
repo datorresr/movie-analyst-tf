683e6c96b4432171bb366495191eea826cef65ee
datorresr <datorresr@gmail.com>
2023-11-03 17:41:34 -0500

    providers

--- Production/terraform.tf
+++ Production/terraform.tf
@@ -1,30 +1,4 @@
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      //version = "~> 4.46.0"
-    }
-
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.4.3"
-    }
-
-    tls = {
-      source  = "hashicorp/tls"
-      version = "~> 4.0.4"
-    }
-
-    cloudinit = {
-      source  = "hashicorp/cloudinit"
-      version = "~> 2.2.0"
-    }
-
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.23.0"
-    }
-  }
 
   backend "s3" {
     # Replace this with your bucket name!
@@ -37,5 +11,5 @@ terraform {
     encrypt        = true
   }
 
-  required_version = "~> 1.5"
+
 }
